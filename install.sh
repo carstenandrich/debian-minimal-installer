211521269ab0112e4dc88a9e4be920f0f794eb3b
Carsten Andrich <carsten@andrich.name>
2022-10-29 22:48:41 +0200

    refactor build process to support Bullseye and Sid with systemd-boot

--- install.sh
+++ install.sh
@@ -1,154 +1,174 @@
 #!/bin/sh -eux
 
-# apt install btrfs-progs cdebootstrap dosfstools
+# apt install bubblewrap btrfs-progs cdebootstrap dosfstools fdisk
+
+### vvv ADJUST THESE PARAMETERS vvv ###
 
 # configure
-APT_MIRROR="http://deb.debian.org/debian"
-DEBIAN_SUITE="bullseye"
+DEBIAN_SUITE="sid"
 HOSTNAME="debian"
 HOSTNAME_FQDN=""
-DEV="/dev/nvme0n1"
-DEV_ESP="/dev/nvme0n1p1"
-ROOT_DEV="/dev/nvme0n1p2"
-ROOT_MOUNT="/mnt/root"
-XKBMODEL="pc105"
-XKBLAYOUT="de"
+DEV="/dev/sdb"
+# TODO: determine devices automatically
+DEV_ESP="${DEV}1"
+DEV_ROOT="${DEV}2"
 
-unmount()
-{
-        # unmount bind mounts created by build process
-        for mount in "$ROOT_MOUNT/@root/boot/efi" "$ROOT_MOUNT/@root/mnt/root" "$ROOT_MOUNT/@root/proc" "$ROOT_MOUNT/@root/sys/firmware/efi/efivars" "$ROOT_MOUNT/@root/sys" "$ROOT_MOUNT/@root/dev/pts" "$ROOT_MOUNT/@root/dev" ; do
-                if mountpoint -q "$mount" ; then
-                        umount "$mount"
-                fi
-        done
-}
+# pack include files with proper ownership
+rm -f include.tar.gz
+tar -czf include.tar.gz --owner=root:0 --group=root:0 -C include etc root
 
-# don't touch
-INSTALL_ROOT="$ROOT_MOUNT/@root"
-
-# call and trap register unmount()
-unmount
-trap "unmount" EXIT INT
-
-# create partition table
-sfdisk $DEV <<-EOF
+# create partition table (must have separate esp and root partitions)
+# FIXME: overwrites existing partition table without confirmation!!!
+sfdisk --lock $DEV <<-EOF
 	label: gpt
 
 	name=esp,  size=1G, type=uefi, bootable
 	name=root, size=4G, type=linux
 EOF
 
-# wait for devices to be created
-while [ ! -e $DEV_ESP ] ; do
-	sleep 0.1
-done
-while [ ! -e $ROOT_DEV ] ; do
-	sleep 0.1
-done
-# workaround for spurious mkfs.fat failures due to missing device
-sleep 1
+### ^^^ ADJUST THESE PARAMETERS ^^^ ###
 
-# create EFI system partition
+# register function for reliable cleanup when script exits (both regular exit
+# and premature termination due to errors, signals, etc.)
+cleanup()
+{
+        # unmount bind mounts created by build process
+        for mount in root.mnt/@root/boot/efi root.mnt ; do
+                if mountpoint -q "$mount" ; then
+                        umount "$mount"
+                fi
+        done
+
+	if [ -d root.mnt ] ; then
+		rmdir root.mnt
+	fi
+}
+trap "cleanup" EXIT INT
+
+# wait for udev to create device nodes
+udevadm settle
+
+# create btrfs filesystem, mount it, and create subvolumes
+# FIXME: fails if $DEV_ROOT contains a partition signature
+mkfs.btrfs --label root $DEV_ROOT
+UUID_ROOT=$(blkid --match-tag UUID --output value $DEV_ROOT)
+mkdir root.mnt
+mount $DEV_ROOT root.mnt
+btrfs subvolume create root.mnt/@root
+btrfs subvolume create root.mnt/@home
+btrfs subvolume set-default root.mnt/@root
+mkdir -p root.mnt/@root/mnt/root
+
+# create EFI system partition and mount it
 mkfs.fat -F 32 $DEV_ESP
 UUID_ESP=$(blkid --match-tag UUID --output value $DEV_ESP)
+mkdir -p root.mnt/@root/boot/efi
+mount $DEV_ESP root.mnt/@root/boot/efi
 
-# create btrfs filesystem, mount it, and create default subvolume
-mkfs.btrfs --label root --metadata single $ROOT_DEV
-ROOT_UUID=$(blkid --match-tag UUID --output value $ROOT_DEV)
-mkdir -p $ROOT_MOUNT
-mount $ROOT_DEV $ROOT_MOUNT
-btrfs subvolume create $ROOT_MOUNT/@root
-btrfs subvolume set-default $ROOT_MOUNT/@root
-
-# setup read-only btrfs snapshots
-mkdir -p $ROOT_MOUNT/.snapshots/root
-cat >$ROOT_MOUNT/mksnapshot_root.sh <<-EOF
-	#!/bin/sh -eux
-
-	btrfs subvol snapshot -r $ROOT_MOUNT/@root $ROOT_MOUNT/.snapshots/root/@\$(date --utc +%Y-%m-%dT%H%M%SZ)
-EOF
-chmod 700 $ROOT_MOUNT/mksnapshot_root.sh $ROOT_MOUNT/.snapshots $ROOT_MOUNT/.snapshots/root
-
-# bootstrap system
-cdebootstrap --flavour=minimal --include=whiptail $DEBIAN_SUITE $ROOT_MOUNT/@root $APT_MIRROR | tee bootstrap.log 2>&1
-$ROOT_MOUNT/mksnapshot_root.sh
-
-# replace debootstrap's /etc/apt/sources.list with more complete alternative
-if [ "$DEBIAN_SUITE" = "sid" ] ; then
-	echo "deb http://deb.debian.org/debian/ sid main contrib non-free" > $ROOT_MOUNT/@root/etc/apt/sources.list
+# bootstrap system, caching bootstrap results to accelerate potential rebuilds
+if [ -f bootstrap.tar.gz ] ; then
+	tar -xf bootstrap.tar.gz -C root.mnt/@root
 else
-	echo "deb http://deb.debian.org/debian $DEBIAN_SUITE main contrib non-free" > $ROOT_MOUNT/@root/etc/apt/sources.list
-	echo "#deb http://security.debian.org/ $DEBIAN_SUITE/updates main contrib non-free" >> $ROOT_MOUNT/@root/etc/apt/sources.list
-	echo "#deb http://deb.debian.org/debian $DEBIAN_SUITE-updates main contrib non-free" >> $ROOT_MOUNT/@root/etc/apt/sources.list
-	echo "#deb http://deb.debian.org/debian $DEBIAN_SUITE-backports main contrib non-free" >> $ROOT_MOUNT/@root/etc/apt/sources.list
+	cdebootstrap --flavour=minimal --include=usrmerge,whiptail $DEBIAN_SUITE root.mnt/@root "http://deb.debian.org/debian"
+	tar -czf bootstrap.tar.gz -C root.mnt/@root .
+fi
+
+# update resolv.conf (may have changed since bootstrapping)
+cat /etc/resolv.conf > root.mnt/@root/etc/resolv.conf
+
+# configure apt sources
+echo "deb http://deb.debian.org/debian $DEBIAN_SUITE main contrib non-free" > root.mnt/@root/etc/apt/sources.list
+if [ $DEBIAN_SUITE != "sid" ] ; then
+	echo "deb http://security.debian.org/debian-security $DEBIAN_SUITE-security main contrib non-free" >> root.mnt/@root/etc/apt/sources.list
+	echo "deb http://deb.debian.org/debian $DEBIAN_SUITE-updates main contrib non-free" >> root.mnt/@root/etc/apt/sources.list
+	echo "deb http://deb.debian.org/debian $DEBIAN_SUITE-backports main contrib non-free" >> root.mnt/@root/etc/apt/sources.list
 fi
 
 # create /etc/hostname and /etc/hosts
-echo "127.0.0.1 localhost" > $ROOT_MOUNT/@root/etc/hosts
+echo "127.0.0.1 localhost" > root.mnt/@root/etc/hosts
 if [ -n "$HOSTNAME_FQDN" ] ; then
-	echo "$HOSTNAME_FQDN" > $ROOT_MOUNT/@root/etc/hostname
-	echo "127.0.1.1 $HOSTNAME_FQDN $HOSTNAME" >> $ROOT_MOUNT/@root/etc/hosts
+	echo "$HOSTNAME_FQDN" > root.mnt/@root/etc/hostname
+	echo "127.0.1.1 $HOSTNAME_FQDN $HOSTNAME" >> root.mnt/@root/etc/hosts
 else
-	echo "$HOSTNAME" > $ROOT_MOUNT/@root/etc/hostname
-	echo "127.0.1.1 $HOSTNAME" >> $ROOT_MOUNT/@root/etc/hosts
+	echo "$HOSTNAME" > root.mnt/@root/etc/hostname
+	echo "127.0.1.1 $HOSTNAME" >> root.mnt/@root/etc/hosts
 fi
 
 # create /etc/fstab
-cat >$ROOT_MOUNT/@root/etc/fstab <<-EOF
-	UUID="$ROOT_UUID" /         btrfs relatime,ssd            0 0
-	UUID="$ROOT_UUID" /mnt/root btrfs relatime,ssd,subvolid=5 0 0
-	UUID="$UUID_ESP"                            /boot/efi vfat  relatime                0 0
-	tmpfs                                       /tmp      tmpfs mode=1777               0 0
-EOF
-
-# configure keyboard layout
-cat > $ROOT_MOUNT/@root/etc/default/keyboard <<-EOF
-	# KEYBOARD CONFIGURATION FILE
-
-	# Consult the keyboard(5) manual page.
-
-	XKBMODEL="$XKBMODEL"
-	XKBLAYOUT="$XKBLAYOUT"
-	XKBVARIANT=""
-	XKBOPTIONS=""
-
-	BACKSPACE="guess"
+cat >root.mnt/@root/etc/fstab <<-EOF
+	UUID="$UUID_ROOT" /         btrfs relatime,ssd              0 0
+	UUID="$UUID_ROOT" /home     btrfs relatime,ssd,subvol=@home 0 0
+	UUID="$UUID_ROOT" /mnt/root btrfs relatime,ssd,subvolid=5   0 0
+	UUID="$UUID_ESP"                            /boot/efi vfat  relatime                  0 0
+	tmpfs                                       /tmp      tmpfs mode=1777                 0 0
 EOF
 
 # create /etc/kernel/cmdline
-echo "root=UUID=$ROOT_UUID ro" > $ROOT_MOUNT/@root/etc/kernel/cmdline
+echo "root=UUID=$UUID_ROOT ro" > root.mnt/@root/etc/kernel/cmdline
 
 # untar includes
-tar -xf include.tar.gz -C $ROOT_MOUNT/@root
+tar -xf include.tar.gz -C root.mnt/@root
 
-# bind mount
-mkdir $ROOT_MOUNT/@root/mnt/root $ROOT_MOUNT/@root/boot/efi
-mount $DEV_ESP $ROOT_MOUNT/@root/boot/efi
-mount --bind /dev $ROOT_MOUNT/@root/dev
-mount --bind /dev/pts $ROOT_MOUNT/@root/dev/pts
-mount --bind /sys $ROOT_MOUNT/@root/sys
-mount --bind /sys/firmware/efi/efivars $ROOT_MOUNT/@root/sys/firmware/efi/efivars
-mount --bind /proc $ROOT_MOUNT/@root/proc
-mount --bind $ROOT_MOUNT $ROOT_MOUNT/@root/mnt/root
+# build systemd-boot package (required for automatic updates on Bullseye)
+# TODO: remove when deprecating Bullseye
+dpkg-deb -b systemd-boot/
 
-# call chroot build script with clean environment (to prevent locale issues, etc.)
-cp install_chroot.sh $ROOT_MOUNT/@root/
-env --ignore-environment \
-	PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" \
-	TERM="$TERM" USER="$USER" \
-	chroot $ROOT_MOUNT/@root /install_chroot.sh
-rm $ROOT_MOUNT/@root/install_chroot.sh
-
-# install hook that updates systemd-boot loader configs on installation of kernel images
-dpkg --root=$ROOT_MOUNT/@root --install update-systemd-boot.deb
-
-# unmount
-unmount
+# call chroot build script via bubblewrap:
+#   * clears environment variables (prevents locale issues)
+#   * binds /var/cache/apt/archives into chroot to cache downloaded .deb files
+#   * partially isolates host system from build process (via minimally
+#     populated /dev and /proc mounts)
+#   * cleans up reliably (unmount everything and kill any remaining processes)
+#
+# TODO: remove /dev/block when deprecating Bullseye
+# TODO: remove systemd-boot.deb when deprecating Bullseye
+mkdir -p /var/cache/apt/archives
+bwrap \
+	--clearenv \
+	--setenv DEBIAN_SUITE "$DEBIAN_SUITE" \
+	--setenv HOME "$HOME" \
+	--setenv PATH "/usr/sbin:/usr/bin:/sbin:/bin" \
+	--setenv TERM "$TERM" \
+	--setenv USER "$USER" \
+	\
+	--bind root.mnt/@root / \
+	--bind root.mnt/@home /home \
+	--dev /dev \
+	--ro-bind /dev/block /dev/block \
+	--dev-bind $DEV $DEV \
+	--dev-bind $DEV_ROOT $DEV_ROOT \
+	--dev-bind $DEV_ESP $DEV_ESP \
+	--bind root.mnt /mnt/root \
+	--proc /proc \
+	--tmpfs /run \
+	--ro-bind /sys /sys \
+	--bind /sys/firmware/efi/efivars /sys/firmware/efi/efivars \
+	--perms 1777 --tmpfs /tmp \
+	--perms 500 --file 3 /tmp/install_chroot.sh \
+	--perms 644 --file 4 /tmp/systemd-boot.deb \
+	--bind /var/cache/apt/archives /var/cache/apt/archives \
+	\
+	--unshare-pid --die-with-parent \
+	/tmp/install_chroot.sh \
+		3<install_chroot.sh \
+		4<systemd-boot.deb
 
 # remove chroot helper that disables service invocation
-dpkg --root=$ROOT_MOUNT/@root --purge cdebootstrap-helper-rc.d
+dpkg --root=root.mnt/@root --purge cdebootstrap-helper-rc.d
 
-# create snapshot and unmount
-$ROOT_MOUNT/mksnapshot_root.sh
-umount $ROOT_MOUNT
+# create snapshots
+mkdir -p root.mnt/.snapshots/home root.mnt/.snapshots/root
+chmod 700 root.mnt/.snapshots root.mnt/.snapshots/home root.mnt/.snapshots/root
+btrfs subvol snapshot -r root.mnt/@home root.mnt/.snapshots/home/@$(date --utc +%Y-%m-%dT%H%M%SZ)
+btrfs subvol snapshot -r root.mnt/@root root.mnt/.snapshots/root/@$(date --utc +%Y-%m-%dT%H%M%SZ)
+
+# create snapshot script
+cat >root.mnt/mksnapshot.sh <<-EOF
+	#!/bin/sh -eux
+
+	btrfs subvol snapshot -r /mnt/root/@home /mnt/root/.snapshots/home/@\$(date --utc +%Y-%m-%dT%H%M%SZ)
+	btrfs subvol snapshot -r /mnt/root/@root /mnt/root/.snapshots/root/@\$(date --utc +%Y-%m-%dT%H%M%SZ)
+EOF
+chmod +x root.mnt/mksnapshot.sh
+
+# cleanup() will be called by EXIT trap
--- install_chroot.sh
+++ install_chroot.sh
@@ -8,54 +8,85 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
 # setup C locale as default
-apt-get --assume-yes install locales
+apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
+	locales
 update-locale LANG=C.UTF-8
 
-# install subset of important packages plus some personal favorites
-# FIXME: as of systemd package version 251.2-3, systemd-boot was split off into separate packaged, see:
-#        https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.2-3/debian/changelog
-#        the systemd-boot package does not exist on current Debian stable (Bullseye) or prior versions
-apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
-	bsdmainutils cpio dbus dmidecode init iproute2 iputils-ping \
-	kmod mount nano netbase sensible-utils systemd systemd-boot systemd-sysv tzdata udev \
-	vim-common vim-tiny
+# systemd-boot packaged separately since Debian Bookworm/Sid (systemd >= 251.2-3)
+# https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.2-3/debian/changelog
+# TODO: remove after stable release of Bookworm
+if [ $DEBIAN_SUITE = "bookworm" -o $DEBIAN_SUITE = "sid" ] ; then
+	systemd_boot="systemd-boot"
+else
+	# inofficial backport of systemd-boot package's kernel update hooks
+	systemd_boot="/tmp/systemd-boot.deb"
+fi
 
-# install remaining packages
-apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
-	busybox console-setup keyboard-configuration linux-image-amd64 \
-	\
-	bash-completion file htop less man-db manpages psmisc screen sudo \
-	pciutils usbutils \
-	\
-	ethtool iputils-arping iputils-tracepath netcat-openbsd openssh-client \
-	rsync tcpdump \
-	\
-	btrfs-progs dosfstools e2fsprogs fdisk \
-	\
-	openssh-server
+# systemd-resolved packaged separately since Debian Bookworm/Sid (systemd >= 252.3-2)
+# https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.3-2/debian/systemd.NEWS
+# TODO: remove after stable release of Bookworm
+if [ $DEBIAN_SUITE = "bookworm" -o $DEBIAN_SUITE = "sid" ] ; then
+	systemd_resolved="systemd-resolved"
+fi
 
-# use local keymap in initramfs (busybox)
+# installing systemd-resolved replaces /etc/resolv.conf with a symlink to
+# /run/system/resolved/stub-resolv.conf. with service invocation inhibited in
+# the chroot, systemd-resolved does not run and these files are missing,
+# breaking DNS resolution. as a workaround, substitute these files with the
+# contents of /etc/resolv.conf (expected to be a usable resolv.conf provided
+# by the host)
+mkdir -p /run/systemd/resolve
+cat /etc/resolv.conf > /run/systemd/resolve/resolv.conf
+cat /etc/resolv.conf > /run/systemd/resolve/stub-resolv.conf
+
+# install all packages except kernel (avoids multiple initramfs rebuilds)
+apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
+	bsdmainutils cpio dbus dmidecode init initramfs-tools iproute2 \
+	kmod mount nano netbase sensible-utils \
+	systemd ${systemd_boot:-} ${systemd_resolved:-} systemd-sysv systemd-timesyncd \
+	tzdata udev vim-tiny zstd \
+	\
+	bash-completion busybox console-setup keyboard-configuration usb-modeswitch \
+	htop less man-db manpages \
+	btrfs-progs dosfstools fdisk \
+	iputils-ping iputils-tracepath netcat-openbsd openssh-client openssh-server
+
+# enable systemd services not enabled by default
+systemctl enable systemd-boot-update.service
+systemctl enable systemd-networkd.service
+
+# enable systemd-resolved service and use its resolv.conf on Bullseye
+# (happens via systemd-resolved's postinst script on Bookworm/Sid)
+# TODO: remove after stable release of Bookworm
+if [ $DEBIAN_SUITE = "bullseye" ] ; then
+	systemctl enable systemd-resolved.service
+	ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+fi
+
+# disable resuming (emits warning during initramfs generation and may cause
+# boot delay when erroneously waiting for swap partition)
+# https://manpages.debian.org/bullseye/initramfs-tools-core/initramfs-tools.7.en.html#resume
+echo "RESUME=none" > /etc/initramfs-tools/conf.d/resume
+
+# configure initramfs generation before installing kernel
+# (use local keymap in initramfs)
 sed -i 's/^KEYMAP=n/KEYMAP=y/' /etc/initramfs-tools/initramfs.conf
-dpkg-reconfigure initramfs-tools
 
-# install and configure boot loader (systemd-boot)
+# install and configure systemd-boot before installing kernel
 bootctl --esp-path=/boot/efi install
+# bootctl does not create system token in VMs, so create directory explicitly
+mkdir -p /boot/efi/$(cat /etc/machine-id)
 cat >/boot/efi/loader/loader.conf <<-EOF
 	default $(cat /etc/machine-id)-*
 	timeout 5
 EOF
 
-# use systemd services
-systemctl enable systemd-networkd.service
-systemctl enable systemd-resolved.service
-systemctl enable systemd-timesyncd.service
+# install kernel
+apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
+	linux-image-amd64
 
-# use systemd-resolved's resolv.conf instead of the one generated by debootstrap
-ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
-# enable peristent systemd journal (disable copy-on-write for journal files)
+# enable persistent systemd journal
 mkdir -p /var/log/journal
-chattr +C /var/log/journal
 systemd-tmpfiles --create --prefix /var/log/journal
 
 # set root password "root" (generate via `openssl passwd -1 -salt ""`)
